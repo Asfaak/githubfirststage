3539b177cf05cb810dd79b712ff5a0b217c84831
Asfaak <156909868+Asfaak@users.noreply.github.com>
2025-08-15 23:47:16 +0530

    Update test4.java

--- test4.java
+++ test4.java
@@ -1,5 +1,6 @@
 public class Main{
 public static  void main(String args){
     System.out.println("hi mcn yeppadiyachchum thappiranum da");
+    System.out.println("ippo change pannatha parkka poram   v v v va v vav va ");
 } 
 }
